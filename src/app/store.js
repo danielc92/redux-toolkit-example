fc71d71930ad4de05e48a5b67be85ba3a6e829f0
Daniel Corcoran <danielc.92@outlook.com>
2020-05-12 21:23:05 +1000

    Add RootState

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-
-export default configureStore({
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import todoReducer from "../features/todos/todosSlice";
+ 
+const store = configureStore({
   reducer: {
     counter: counterReducer,
+    todos: todoReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>
+
+export default store
